339eddbf6423cacfe9ab0ae7b458fee6c07f6136
Jed Northridge <git@jedcn.com>
2014-12-08 09:03:36 -0500

    [homebrew] Added zsh + setup my config

--- modules/people/manifests/jedcn.pp
+++ modules/people/manifests/jedcn.pp
@@ -63,4 +63,44 @@ class people::jedcn {
     source  => 'jedcn/emacs-setup',
     require => File[$my_init_src]
   }
+
+  ########################################
+  # ZSH
+  ########################################
+
+  # Get ZSH from brew-- it's likely more recent than whatever Apple
+  # ships.
+  package { 'zsh':
+    ensure => present,
+  }
+
+  # Change the default shell to the zsh version above.
+  osx_chsh { $::luser:
+    shell   => '/opt/boxen/homebrew/bin/zsh',
+    require => Package['zsh'],
+  }
+
+  file_line { 'add zsh to /etc/shells':
+    path    => '/etc/shells',
+    line    => "${boxen::config::homebrewdir}/bin/zsh",
+    require => Package['zsh'],
+  }
+
+  file { "/Users/${luser}/.zshrc":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${my_init_src}/dot-org-files/home/.zshrc",
+    require => Repository["${my_init_src}/dot-org-files"],
+  }
+
+  repository { "${my_init_src}/oh-my-zsh":
+    source  => 'robbyrussell/oh-my-zsh',
+    require => File[$my_init_src]
+  }
+
+  file { "/Users/${luser}/.oh-my-zsh":
+    ensure  => link,
+    target  => "${my_init_src}/oh-my-zsh",
+    require => Repository["${my_init_src}/oh-my-zsh"],
+  }
 }
