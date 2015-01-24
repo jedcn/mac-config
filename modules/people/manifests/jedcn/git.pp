c20c8c23faa6ae2f4408705d23efc015995ee5db
Jed Northridge <git@jedcn.com>
2015-01-23 22:46:56 -0500

    [git] Setup 'simple' push defaults

--- modules/people/manifests/jedcn/git.pp
+++ modules/people/manifests/jedcn/git.pp
@@ -3,4 +3,7 @@ class people::jedcn::git {
   git::config::global { 'core.editor':
     value  => '/opt/boxen/homebrew/bin/emacsclient'
   }
+  git::config::global { 'push.default':
+    value  => 'simple'
+  }
 }
